b67ef449deb322360606555f852ad850d113e4d6
kondzio-p <konradczater@gmail.com>
2025-09-23 11:20:08 +0200

    23.09

---
+++ cwiczenie1/app/src/main/java/com/example/cwiczenie1/Country.java
@@ -0,0 +1,12 @@
+package com.example.cwiczenie1;
+
+public class Country {
+
+    public String kraj;
+
+    public int liczba_mieszkancow;
+
+    public int powierzchnia;
+
+    public String flaga;
+}
